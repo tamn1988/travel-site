03aeeef56ed8130a5126ea462ae62a667c0e730c
Tam Nguyen <Tamn1988@gmail.com>
2017-12-04 14:23:54 -0800

    added lazyloading

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -4,6 +4,7 @@ import StickyHeader from './modules/StickyHeader';
 import $ from 'jquery';
 import Modal from './modules/Modal';
 
+
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonial'), '60%');
